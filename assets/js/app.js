9f90262d1478c52e19ff1d787674dc2dac106c95
Ibrahim Yahya <yahyalito1@gmail.com>
2022-07-04 13:04:04 +0300

    Selecting elements from the DOM
    Selecting elements from the DOM

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+// Selecting elements from the DOM
+const searchButton = document.querySelector('#search');;
+const searchInput = document.querySelector('#exampleInputEmail1');
+const moviesContainer = document.querySelector('#movies-container');
+const moviesSearchable = document.querySelector('#movies-searchable');
